2ffb6233bffde92477ceaaf3e500bc19081e0375
anandkr7 <anandkr7@gmail.com>
2018-11-25 19:20:42 +0530

    Adding a sample test case...

---
+++ src/test/java/com/upgrad/saavn/BasicTest.java
@@ -0,0 +1,18 @@
+package com.upgrad.saavn;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BasicTest {
+
+	@Test
+	public void testBasic() {
+
+		int first = 1;
+		int second = 2;
+
+		Assert.assertTrue("Invalid sum", (first + second) == 3);
+
+	}
+
+}
